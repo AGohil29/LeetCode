16a9da6da5e86c0a537d4ecf8aad5173d74d8d16
ArunGoldmedal <104761830+ArunGoldmedal@users.noreply.github.com>
2025-08-20 15:02:04 +0530

    Add 189 Rotate Array alternate solutions

--- app/src/main/java/com/example/goldmedal/leetcode/array/RotateArray.kt
+++ app/src/main/java/com/example/goldmedal/leetcode/array/RotateArray.kt
@@ -1,11 +1,11 @@
 package com.example.goldmedal.leetcode.array
 
 fun main() {
-    val nums = intArrayOf(1, 2, 3, 4, 5, 6, 7)
+    val nums = intArrayOf(1, 2, 3, 4, 5, 6)
     println("Original Array: ${nums.contentToString()}")
 
-    val newNums = RotateArray().rotate(nums, 3)
-    println("Original Array: ${newNums}")
+    RotateArray().rotateArrFourth(nums, 2)
+    println("New Array: ${nums.contentToString()}")
 }
 
 /*
@@ -42,5 +42,94 @@ class RotateArray {
             i++
             j--
         }
+    }
+
+    // Solution 2: rotate array 1 at a time d times
+    fun rotateArr(nums: IntArray, d: Int) {
+        val n = nums.size
+
+        // repeat the rotation d times
+        for (i in 0 until d) {
+            val last = nums[n-1]
+
+            for (j in n-1 downTo 1) {
+                nums[j] = nums[j-1]
+            }
+            nums[0] = last
+        }
+    }
+
+    // Solution 3: Using temporary array
+    /*
+    * 1. Create a temp array
+    * 2. First copy last d elements to start d elements of temp array
+    * 3. Copy first n-d elements to last n-d element of temp array
+    * 4. Finally copy all elements for temp array to original array
+    * Input: nums = [1,2,3,4,5,6,7], k = 3
+    * Output: [5,6,7,1,2,3,4]
+    * */
+    fun rotateArrThird(nums: IntArray, k: Int) {
+        val n = nums.size
+
+        val d = k % n
+        // step 1
+        val tempNums = IntArray(n)
+
+        // step 2
+        for (i in 0 until d) {
+            tempNums[i] = nums[n-d+i]
+        }
+        println("1. Temp array: ${tempNums.contentToString()}")
+
+        // step 3
+        for (i in d until n)
+            tempNums[i] = nums[i-d]
+
+        println("2. Temp array: ${tempNums.contentToString()}")
+
+        // step 4
+        for (i in 0 until n)
+            nums[i] = tempNums[i]
+        println("3. Temp array: ${tempNums.contentToString()}")
+    }
+
+    // Solution 4: Juggling Algorithm
+    /*
+    *  Use concept of cycles
+    * Input: nums = [1,2,3,4,5,6,7], k = 3
+    * Output: [5,6,7,1,2,3,4]
+    * */
+    fun rotateArrFourth(nums: IntArray, k: Int) {
+        val n = nums.size
+        val d = k % n
+
+        val cycles = gcd(n, d)
+
+        for (i in 0 until cycles) {
+            var currIdx = i
+            var currEle = nums[currIdx]
+
+            do {
+                val nextIdx = (currIdx + d) % n
+                val nextEle = nums[nextIdx]
+
+                nums[nextIdx] = currEle
+
+                currEle = nextEle
+                currIdx = nextIdx
+            } while (currIdx != i)
+        }
+    }
+
+    private fun gcd(a: Int, b: Int): Int {
+        var x = a
+        var y = b
+
+        while (y != 0) {
+            val temp = y
+            y = x % y
+            x = temp
+        }
+        return x
     }
 }
